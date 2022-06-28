9a7c8032ec4c1d498792557957f69ffbc7db2d94
Tran Khai Hoang <hoangbo74@gmail.com>
2022-06-28 14:38:45 +0700

    change views path

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }))
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.set('views', path.join(__dirname, 'resources/views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
